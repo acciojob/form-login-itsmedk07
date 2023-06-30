45f9c5d2fc4282b9d749f091eb7bb52ec7e62263
itsmedk07 <126004945+itsmedk07@users.noreply.github.com>
2023-06-30 20:54:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function getFormvalue() {
     //Write your code here
-	function getFormvalue() {
-    //Write your code here
+	
  const Formele=document.getElementById("form1");
 	const name=Formele.elements['fname'].value;
 	const lname=Formele.elements['lname'].value;
